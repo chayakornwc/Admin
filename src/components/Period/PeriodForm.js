fb69f2c41eed0cbf28a0034d1a1cdc63e8953794
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2018-03-21 11:31:17 +0700

    initial edit

--- src/components/Period/PeriodForm.js
+++ src/components/Period/PeriodForm.js
@@ -1,22 +1,106 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { Button, ModalBody, ModalFooter, ModalHeader, Modal } from 'reactstrap';
+import { Field, reduxForm } from 'redux-form';
+import renderField from '../Utils/renderFields';
 class PeriodForm extends Component {
     constructor(props){
         super(props);
         
     }
+    componentDidMount(){
+        this.handleInitialize();
+    }
+    handleInitialize(){ 
+        let initData = {
+            course_id:'',
+            course_status:null,
+            course_name:'',
+            course_nameEng:'',
+            course_detail:''
+        }
+            if(this.props.data.course_id){
+                initData = this.props.data
+                //status ที่รับมาเป็น init แต่value ต้องแปลงเป็น string ก่อน
+                initData.course_status = this.props.data.course_status.toString()
+            }
+            // method of redux-form
+           
+            this.props.initialize(initData);
+    }
+        toggle = () => {
+            this.props.onToggle();
+        }
+        onSubmit = (values) => {
+            this.props.onSubmit(values);
+        }
+    
     render() {
+        // handleSubmit  properties of redux form
+        const { data, periodSave, onSubmit, handleSubmit , modalTitle, onToggle} = this.props
         return (
             <div>
-                
+                <ModalHeader toggle={onToggle}>{modalTitle}</ModalHeader>
+                 <ModalBody>
+                    {/* ตรวจสอบว่ามี err หรือไม่ */}
+                    {periodSave.isRejected && <div className="alert alert-danger">{periodSave.data}</div>}
+
+                    {/* รูปแบบการแสดงผลจัดตาม Bootstrap 4 */}
+                    <div className="form-group row">
+                        <label className="col-sm-3 col-form-label">สถานะ</label>
+                        <div className="col-sm-9">
+                            <div className="form-check form-check-inline">
+                                <label className="form-check-label">
+                                    <Field name="course_status"  component="input" type="radio" selected value="0"/>{' '}ระงับการใช้งาน
+                                    </label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <label className="form-check-label">
+                                    <Field name="course_status" component="input" type="radio" value="1"/>{' '}เปิดการใช้งาน 
+                                    </label>
+                            </div>
+                        </div>
+                    </div>
+                    <Field name="course_name" component={renderField}  type="text" label="ชื่อหลักสูตร" autoFocus />
+                    <Field name="course_nameEng" component={renderField}  type="text" label="ชื่อภาษาอังกฤษ" />
+                    <Field name="course_detail" component={renderField} textarea type="text" label="รายละเอียด" />
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button color="primary" onClick={handleSubmit(this.onSubmit)}>บันทึก</Button>{' '}
+                    <Button color="secondary" onClick={onToggle}>ยกเลิก</Button>
+                </ModalFooter>
             </div>
         );
     }
+
 }
+
+function validate(values) {
+    const errors = {};
+    if (!values.course_name) {
+        errors.course_name = 'จำเป็นต้องกรอก ฟิลด์นี้!';
+    }
+    if (!values.course_status){
+        errors.course_status = 'จำเป็นต้องเลือก';    
+    }
+    if (!values.course_nameEng) {
+        errors.course_nameEng = 'จำเป็นต้องกรอก ฟิลด์นี้!';
+    }
+
+    return errors;
+}
+
+
 
 PeriodForm.propTypes = {
 
 };
 
-export default PeriodForm;
+
+const form = reduxForm({
+    form: 'PeriodForm',
+    validate
+})
+export default form(PeriodForm)
+
--- src/views/Theme/Coursemanage/Period.js
+++ src/views/Theme/Coursemanage/Period.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { loadPeriods, deletePeriod, savePeriod } from '../../../redux/actions/periodActions';
+import { loadPeriods, deletePeriod, savePeriod, resetStatus, getPeriod } from '../../../redux/actions/periodActions';
 import {connect} from 'react-redux'
 import PeriodTable from '../../../components/period/periodTable';
+import PeriodForm from '../../../components/period/periodForm';
+import {Modal} from 'reactstrap';
 import { confirmModalDialog } from '../../../components/Utils/reactConfirmModalDialog';
+import values from 'redux-form/lib/values';
+const alertify = require('alertify.js');
+
 class Period extends Component {
+
     constructor(props){
         super(props);
-    }
-    state = {modal:false}
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.modalToggle = this.modalToggle.bind(this);
+      }
+
+      state = {modal:false}
+
     componentDidMount(){
-        this.props.dispatch(loadPeriods())
+        this.props.dispatch(loadPeriods());
     }
 
-    ModalToggle(){
+    modalToggle(){
         this.setState({
            modal: !this.state.modal
         })
     }
+    
     handleEdit=(id)=>{
-        
+        this.props.dispatch(resetStatus())
+        this.setState({modalTitle:'แก้ไข'})
+        this.props.dispatch(getPeriod(id)).then(()=>{
+        this.modalToggle();
+        })
     }
     handleDelete=(id)=>{
         confirmModalDialog({
@@ -28,18 +45,30 @@ class Period extends Component {
             confirmLabel:'ยืนยัน ลบทันที',
             message:'คุณต้องการลบข้อมูลนี้ใช่หรือไม่',
             onConfirm: () => this.props.dispatch(deletePeriod(id)).then(() => {
-              this.props.dispatch(loadPeriods())
+                this.props.dispatch(loadPeriods())
               if(!this.props.periodDelete.isRejected){
                   { alertify.alert('ลบข้อมูลหลักสูตรเรียบร้อยแล้ว').set('basic', true)}
               }
               })
             })
-
+    }
+    handleSubmit = (values)=>{
+        this.props.dispatch(savePeriod(values)).then(()=>{
+            if(!this.props.periodSave.isRejected){
+                this.modalToggle();
+                this.props.dispatch.loadPeriods();
+                alertify.alert('แก้ไขข้อมูลเรียบร้อยแล้ว').set('basic', true);
+                
+            }
+        })
     }
     render() {
-        const {periods} = this.props;
+        const {periods, period, periodSave} = this.props;
         return (
             <div className="animated fadeIn">
+              <Modal isOpen={this.state.modal} toggle={this.modalToggle} className="modal-primary" autoFocus={false}>
+                <PeriodForm modalTitle={this.state.modalTitle} data={period.data}  periodSave={periodSave} onSubmit={this.handleSubmit} onToggle={this.modalToggle} />
+            </Modal>
                 <PeriodTable data={periods.data} buttonEdit={this.handleEdit} buttonDelete={this.handleDelete} />
             </div>
         );
