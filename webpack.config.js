5cfe51f30aded9084641e6562224d5b2bffdfc38
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2018-04-20 14:09:28 +0700

    :ok_hand:

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ const SRC_DIR = path.resolve(__dirname, 'src');
 console.log('BUILD_DIR', BUILD_DIR);
 console.log('SRC_DIR', SRC_DIR);
 
+
 module.exports = (env = {}) => {
   return {
     entry: {
