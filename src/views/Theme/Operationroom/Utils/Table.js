e5e49c9e90d7ac39ed3cf2c630b4560e2e0a223a
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2018-05-01 09:47:20 +0700

    :construction:  operation room IS

---
+++ src/views/Theme/Operationroom/Utils/Table.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Badge,Row,Col,Card,CardHeader,CardBody,Table,Pagination,PaginationItem,PaginationLink, Button } from 'reactstrap'
+const moment = require('moment');
+
+export class OPRTable extends Component {
+    constructor(props){
+        super(props);
+    }
+  static propTypes = {
+
+  }
+
+  render() {
+      const {data, ButtonDelete, ButtonEdit} = this.props
+    return (
+      <div>
+           <Row>
+                <Col>
+                    <Card>
+                    <CardHeader>
+                        <i className="icon-note"></i>OPERATION ROOM<a href="/#/course/period/register"><Button className="float-right" color="secondary"><i className="icon-directions"></i>{'\u00A0 Add Operation Room'}</Button></a>
+                    </CardHeader>
+                    <CardBody>
+                    {data && <Table>
+                        <thead>
+                            <tr>
+                                <th>ลำดับ</th>
+                                <th>ชื่อห้องปัฏิบัตการ</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {data.map(function(e, i){  
+                        return(
+                          <tr key={i++}>
+                                <td>{i++}</td>
+                                <td>{e.name}</td>
+                                <td className="ar"><i onClick={()=>ButtonDelete(e.id)} className="fa fa-times"></i></td>
+                            </tr>
+                        )  
+                        })}
+                         </tbody>
+                    </Table>}
+                    </CardBody>
+                   </Card>
+                </Col>
+            </Row>   
+      </div>
+    )
+  }
+}
--- src/views/Theme/Operationroom/index.js
+++ src/views/Theme/Operationroom/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
-
-
 import {connect} from 'react-redux'
+
 import { loadRooms, getRoom, saveRoom, DeleteRoom, resetSaveStatus } from '../../../redux/actions/operationRoomActions';
-export default class Operationroom extends Component {
+import {OPRTable} from './Utils/Table';
+import { Modal } from 'reactstrap';
+
+class Operationroom extends Component {
     constructor(props){
         super(props);
         this.state={
             modal:false,
             modalTitle:''
         }
+        this.modalToggle = this.modalToggle.bind(this);
     }
 
     modalToggle(){
@@ -18,14 +21,27 @@ export default class Operationroom extends Component {
         })
       }
     componentDidMount(){
-
+      this.props.dispatch(loadRooms());
+    }
+    componentWillReceiveProps(nextProps){
+      console.log(nextProps.operation_rooms);
     }
   render() {
-
+      const {operation_rooms, operation_roomDelete, operation_roomSave} = this.props
+      console.log(operation_rooms);
     return (
       <div className="animated fadeIn">
-        
+        <OPRTable data={operation_rooms.data}/>
       </div>
     )
   }
 }
+function mapStateToProps(state) {
+  return{
+       operation_rooms:state.operationRoomReducers.operation_rooms,   
+      operation_roomDelete:state.operationRoomReducers.operation_roomDelete,
+      operation_roomSave:state.operationRoomReducers.operation_roomSave
+   
+  }
+}
+export default connect(mapStateToProps)(Operationroom);
