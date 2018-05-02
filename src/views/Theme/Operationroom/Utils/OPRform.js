4d495647cc04af3f381ac58847d6475cae0b0af0
chayakornwc <whitecat.chayakorn@gmail.com>
2018-05-02 17:04:09 +0700

    :zap: :+1:   Operation room module

--- src/views/Theme/Operationroom/Utils/OPRform.js
+++ src/views/Theme/Operationroom/Utils/OPRform.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { Button, ModalBody, ModalFooter, ModalHeader, Modal, Form, FormGroup, Col, Label } from 'reactstrap';
 
+import { Field, reduxForm } from 'redux-form';
+
+import renderField from '../../../../components/Utils/renderFields';
 class OPRform extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  handleInitialize = ()=> {
+    let initData = {
+      "room_id":parseInt(this.props.data.room_id),
+      "room_name":this.props.data.room_name
+    };
+    this.props.initialize(initData);
+  }
+  toggle = ()=>{
+    this.props.onToggle();
+  }
+  onSubmit = (values)=>{
+    this.props.onSubmit(values);
+  }
+  componentDidMount(){
+    this.handleInitialize();
+  }
   render() {
+    const {data ,operation_roomSave, handleSubmit, onToggle, modalTitle} = this.props
     return (
       <div>
-        
+            <ModalHeader toggle={onToggle}>{modalTitle}</ModalHeader>
+            <ModalBody>
+                    {/* ตรวจสอบว่ามี err หรือไม่ */}
+                    {operation_roomSave.isRejected && <div className="alert alert-danger">{operation_roomSave.data}</div>}
+
+                    {/* รูปแบบการแสดงผลจัดตาม Bootstrap 4 */}
+                    <Form className="form-horizontal">
+                      <FormGroup >
+                        <Field name="room_name" component={renderField}  type="text" label="ห้องปฏิบัติการ" />
+                      </FormGroup>
+                    </Form>  
+            </ModalBody>        
+            <ModalFooter>
+                    <Button color="primary" onClick={handleSubmit(this.onSubmit)}>บันทึก</Button>{' '}
+                    <Button color="secondary" onClick={onToggle}>ยกเลิก</Button>
+                </ModalFooter>
       </div>
     )
   }
 }
-export default OPRform
+
+const form = reduxForm({
+  form: 'OPRform',
+})
+export default form(OPRform)
+
--- src/views/Theme/Operationroom/index.js
+++ src/views/Theme/Operationroom/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-
-import { loadRooms, getRoom, saveRoom, DeleteRoom, resetSaveStatus } from '../../../redux/actions/operationRoomActions';
-import {OPRTable} from './Utils/Table';
 import { Modal } from 'reactstrap';
+import { loadRooms, getRoom, SaveRoom, DeleteRoom, resetSaveStatus } from '../../../redux/actions/operationRoomActions';
+import {OPRTable} from './Utils/Table';
+import OPRform from './Utils/OPRform';
+
 import { confirmModalDialog } from '../../../components/Utils/reactConfirmModalDialog';
 const alertify = require('alertify.js');
 
@@ -40,21 +41,35 @@ class Operationroom extends Component {
           })
   }
   handleEdit=(id)=>{
-    this.props.dispatch(resetStatus())
+    this.props.dispatch(resetSaveStatus())
     this.setState({modalTitle:'แก้ไข'})
-    this.props.dispatch(getPeriod(id)).then(()=>{
+    this.props.dispatch(getRoom(id)).then(()=>{
     this.modalToggle();
         
     })
 }
+handleSubmit = (values)=>{
+  this.props.dispatch(SaveRoom(values)).then(()=>{
+      if(!this.props.operation_roomSave.isRejected){
+          this.modalToggle();
+          this.props.dispatch(loadRooms());
+          alertify.alert('แก้ไขข้อมูลเรียบร้อยแล้ว');
+          
+      }
+  })
+}
+
   render() {
-      const {operation_rooms, operation_roomDelete, operation_roomSave} = this.props
+      const {operation_rooms, operation_roomDelete, operation_roomSave,operation_room} = this.props
      
       if(operation_rooms.isRejected){
         return <h1>{operation_rooms.data}</h1>
       }
     return (
       <div className="animated fadeIn">
+      <Modal isOpen={this.state.modal} toggle={this.modalToggle}>
+      <OPRform onToggle={this.modalToggle} onSubmit={this.handleSubmit} operation_roomSave={operation_roomSave} data={operation_room.data}/>
+      </Modal>
         <OPRTable buttonDelete={this.handleDelete} buttonEdit={this.handleEdit} data={operation_rooms.data}/>
       </div>
     )
@@ -63,6 +78,7 @@ class Operationroom extends Component {
 function mapStateToProps(state) {
   return{
        operation_rooms:state.operationRoomReducers.operation_rooms,   
+       operation_room:state.operationRoomReducers.operation_room,
       operation_roomDelete:state.operationRoomReducers.operation_roomDelete,
       operation_roomSave:state.operationRoomReducers.operation_roomSave
    
